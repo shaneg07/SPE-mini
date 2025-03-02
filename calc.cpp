55aa5872930682949c55dec00b9a838c60cdc52f
shaneg07 <shane2907gomes@gmail.com>
2025-03-02 21:22:31 +0530

    Calculator completed

--- calc.cpp
+++ calc.cpp
@@ -3,54 +3,73 @@
 #include <cctype>
 #include <algorithm>
 #include <string>
-void fact(double *x){
 
-    int i =  (int) *x;
+using namespace std;
 
-    i++;
+int fact(int x){
 
-    *x = -69;
-    
+    int i = 1, n = 1;
+
+    while(i <= x) {
+        n *= i;
+        i++;  
+    }
+     
+    return n;
 }
 
 int main() {
 
-
-    bool e = true;
-
     while(true){
-        std::string ch, i;
-        double ans = 0, inp1, inp2;
-        std::cout << "Enter  number or Q to quit\n";
-        std::cin >> ch;
-        std::transform(ch.begin(), ch.end(), ch.begin(),
-        [](unsigned char c){ return std::tolower(c); });
-        if(ch == "q"){
-            std::cout << "Exiting!\n";
-            return 0;
-        }
-        std::cout << " ● sq- √x\n ● fc- x!\n ● ln- ln(x)\n ● pw- x^b\n";
-        std::cin >>i;
-       
-       
-        ans = std::stoi(ch);
-        if(i == "sq"){
-            ans = pow(ans, 2);
-        }
-        else if(i == "ln"){
-            ans = log(ans);
-        }
-        else if(i == "fc"){
-            fact(&ans);
-        }
-        else if(i == "pw"){
-            
-        }
+        double ans;
+        double x, y;
+        int choice;
+        cout << "\n-----------------MENU----------------\n";
+        cout << "1. Square Root\n";
+        cout << "2. Factorial\n";
+        cout << "3. Log Base 2\n";
+        cout << "4. Power\n";
+        cout << "5. Exit\n";
+        cout << "Enter Choice: ";
+        cin >> choice;
+   
 
-        std::cout << ans << '\n';
+        switch (choice) {
+            case 1:
+                cout << "Enter number: ";
+                cin >> x;
+                ans = sqrt(x);
+                cout << "Result: " << ans << endl;
+                break;
         
-
-      
+            case 2:
+                cout << "Enter number: ";
+                cin >> x;
+                ans = fact(x);
+                cout << "Result: " << ans << endl;
+                break;
+        
+            case 3:
+                cout << "Enter number: ";
+                cin >> x;
+                ans = log2(x);
+                cout << "Result: " << ans << endl;
+                break;
+        
+            case 4:
+                cout << "Enter number: ";
+                cin >> x;
+                cout << "Enter power: ";
+                cin >> y;
+                ans = pow(x, y);
+                cout << "Result: " << ans << endl;
+                break;
+            case 5:
+                exit(0);
+            default:
+                cout << "Invalid choice, please try again." << endl;
+                break;
+            }
     }
 
     return 0;
